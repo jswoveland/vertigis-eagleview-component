72ccdfa4da9ee0ac4dd354b3dc8b225a833dc239
Scott Anderson <sanderson@latitudegeo.com>
2025-05-08 14:10:47 -0700

    tests

--- src/components/EagleViewEmbeddedExplorer/EagleView.tsx
+++ src/components/EagleViewEmbeddedExplorer/EagleView.tsx
@@ -24,15 +24,13 @@
 */
 import type { LayoutElementProperties } from "@vertigis/web/components";
 import { LayoutElement } from "@vertigis/web/components";
-import "./EagleView.css";
-import ButtonGroup from "@vertigis/web/ui/ButtonGroup";
-import IconButton from "@vertigis/web/ui/IconButton";
-import CenterMap from "@vertigis/web/ui/icons/CenterMap";
-import Close from "@vertigis/web/ui/icons/Close";
-import React, { useEffect, useState, useRef, useMemo } from "react";
+import { UIContext } from "@vertigis/web/ui";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import ReactDOM from "react-dom";
-import { EagleViewModel} from ".";
-import {ViewUpdatedEventSource} from "./EagleViewModel";
+
+import "./EagleView.css";
+import type { EagleViewModel } from ".";
+import { ViewUpdatedEventSource } from "./EagleViewModel";
 
 // Required to attached initMap function to window.
 declare global {
@@ -41,6 +39,7 @@ declare global {
 
 export default function EagleView(props: LayoutElementProperties<EagleViewModel>): React.ReactElement {
 
+    const { hostElement } = useContext(UIContext)
     const rootRef = useRef<HTMLDivElement>();
     const { model } = props;
     const ONRECENTER = () => void model.Recenter();
@@ -54,10 +53,11 @@ export default function EagleView(props: LayoutElementProperties<EagleViewModel>
 
     const createScript = (url) => {
         console.log("Creating Script");
-        const s = document.createElement("script");
-        s.id ="embedded-explorer-widget";
+
+        const s = hostElement.ownerDocument.createElement("script");
+        s.id = "embedded-explorer-widget";
         s.src = url;
-        s.type = "text/javascript";
+        s.type = "application/javascript";
         s.crossOrigin = "anonymous|use-credentials";
         s.async = true;
         return s;
@@ -66,15 +66,23 @@ export default function EagleView(props: LayoutElementProperties<EagleViewModel>
     useEffect(() => {
         console.log("EagleView.tsx useEffect()");
 
-        const evScriptElt = document.getElementById("embedded-explorer-widget");
+        const evScriptElt = hostElement.ownerDocument.getElementById("embedded-explorer-widget");
+        const window = hostElement.ownerDocument.defaultView as any;
 
-        if(evScriptElt == null) {
-            // This is required to get the EagleView script running.
-            (window as any).define("React", React);
-            (window as any).define("ReactDOM", ReactDOM);
-            (window as any).define("react-virtualized", undefined);
-            (window as any).define("recharts", undefined);
-
+        if (evScriptElt == null) {
+            if (window.define == null) {
+                window["React"] = React;
+                window["ReactDOM"] = ReactDOM;
+                window["react-virtualized"] = undefined;
+                window["recharts"] = undefined;
+            }
+            else {
+                // This is required to get the EagleView script running.
+                window.define("React", React);
+                window.define("ReactDOM", ReactDOM);
+                window.define("react-virtualized", undefined);
+                window.define("recharts", undefined);
+            }
 
             const embedded_explorer_script = createScript(
                 "https://embedded-explorer.eagleview.com/static/embedded-explorer-widget.js"
@@ -82,12 +90,16 @@ export default function EagleView(props: LayoutElementProperties<EagleViewModel>
 
             //We need to force the necessary modules to load 
             embedded_explorer_script.onload = () => {
-                (window as any).require(["lib"], () => {
-                    void initEmbeddedExplorer();
-                })
+                if (window.require) {
+                    window.require(["lib"], () => {
+                        void initEmbeddedExplorer();
+                    })
+                } else {
+                    void initEmbeddedExplorer()
+                }
             };
-            
-            document.body.appendChild(embedded_explorer_script);
+
+            hostElement.ownerDocument.body.appendChild(embedded_explorer_script);
         }
         else {
             void initEmbeddedExplorer();
@@ -97,48 +109,48 @@ export default function EagleView(props: LayoutElementProperties<EagleViewModel>
         //         void initEmbeddedExplorer();
         //     })
         // }
-    } );
+    });
 
 
     const initEmbeddedExplorer = async () => {
         const center = await model.MercatorizePoint(model.map.view.center);
-        const rotation  = model.map.view.viewpoint.rotation;
-        const zoom = model.map.view.zoom -1;
-        const view = { lonLat: {lat: center.latitude, lon: center.longitude}, rotation, zoom };
+        const rotation = model.map.view.viewpoint.rotation;
+        const zoom = model.map.view.zoom - 1;
+        const view = { lonLat: { lat: center.latitude, lon: center.longitude }, rotation, zoom };
 
-        const widget = document.getElementById("eagle-view-map");
-        if(widget == null) {
-            const container = document.createElement("div");
+        const widget = hostElement.ownerDocument.getElementById("eagle-view-map");
+        if (widget == null) {
+            const container = hostElement.ownerDocument.createElement("div");
             container.setAttribute("id", "eagle-view-map");
             rootRef.current.appendChild(container);
         }
-        if(ev.current != null) {
+        if (ev.current != null) {
             ev.current.destroy();
         }
 
-        ev.current = new window.ev.EmbeddedExplorer();
-        model.e3 = ev.current.mount("eagle-view-map", { apiKey: model.apiKey, view }); 
+        ev.current = new hostElement.ownerDocument.defaultView.ev.EmbeddedExplorer();
+        model.e3 = ev.current.mount("eagle-view-map", { apiKey: model.apiKey, view });
     };
 
     const handleMouseDown = () => {
-     //   setIsDragging(true)
-      }
-    
-      const handleMouseMove = (e) => {
+        //   setIsDragging(true)
+    }
+
+    const handleMouseMove = (e) => {
         if (isDragging) {
-         // isProgrammaticExtentChange.current = false
+            // isProgrammaticExtentChange.current = false
         }
-      }
-    
-      const handleMouseUp = () => {
+    }
+
+    const handleMouseUp = () => {
         //if (isDragging) {
         //  setIsDragging(false)
-          if(model) {
+        if (model) {
             model.mapUpdateEventSource = ViewUpdatedEventSource.e3;
-          }
+        }
         //}
-      };
-    
+    };
+
     return (
         <LayoutElement {...props} stretch>
             <div ref={rootRef} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove} onMouseDown={handleMouseDown} onWheel={handleMouseUp} ></div>
